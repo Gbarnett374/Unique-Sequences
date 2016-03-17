dbca15874fa8cdfc4cbcac70f67761eded8bdaa4
Gbarnett374 <Gbarnett374@gmail.com>
2016-03-17 16:34:41 -0400

    Added some doc blocks

--- lib/fileHelper.php
+++ lib/fileHelper.php
@@ -11,7 +11,9 @@ class fileHelper
     $this->location = $location;
     $this->option   = $option;
   }
-
+/**
+ * Opens a file.
+ */
   public function open()
   {
     $this->file = fopen($this->location, $this->option);
@@ -20,31 +22,45 @@ class fileHelper
       throw new Exception("Not a valid resource.");
     }
   }
-
+/**
+ * Reads a single line from a file.
+ * @return array contains the line of data.
+ */
   public function readLine()
   {
     return $line = fgetcsv($this->file);
   }
-
+/**
+ * Writes data to a file.
+ * @param  string $data the word or sequence to be written to the file.
+ */
   public function write($data)
   {
     if (!fwrite($this->file, $data)) {
       throw new Exception("Unable to write the file.");
     }
   }
-
+/**
+ * Closes the file. 
+ */
   public function close()
   {
     if (!fclose($this->file)) {
       throw new Exception("Unable to close the file");
     }
   }
-
+/**
+ * Returns the file property.
+ * @return Resource the handle on the file.
+ */
   public function getFileResource()
   {
     return $this->file;
   }
-  
+/**
+ * Checks to make sure the file is of type Resource.
+ * @return boolean
+ */
   protected function isValidResource()
   {
     return is_resource($this->file) ? true : false;
--- lib/wordHelper.php
+++ lib/wordHelper.php
@@ -8,9 +8,11 @@ class wordHelper
   {
     $this->wordArray = [];
   }
-/* Dertermine the sequences for a word. 
-   The array is keyed by the determined sequence. 
-   Original fullwords are pushed to the array.*/
+/*
+  * Determines the sequences in a word.
+  * Stores the sequence in the key of the array, and the orignal word as the value.
+  * @param  array $word  word from the file to be parsed.
+*/
   public function determineSequences($word)
   {
     if (!$this->validLength($word[0], 3)) {
@@ -25,12 +27,19 @@ class wordHelper
       $this->wordArray[$wordSegment][] = $word[0];
     }
   }
-
+/**
+ * Removes white space and checks that a word is a valid length.
+ * @param  string $word   the word, or sequence of characters. 
+ * @param  int    $length the length the string must be greater than.
+ * @return bool   
+ */
   protected function validLength($word, $length)
   {
     return strlen(trim($word)) > $length ? true : false;
   }
-
+/**
+ * Removes dupes from the array. 
+ */
   public function removeDupes()
   {
     foreach ($this->wordArray as $k => $v) {
@@ -39,12 +48,17 @@ class wordHelper
       }
     }
   }
-
+/**
+ * Sorts the wordArray alphabetically. 
+ */
   public function sort()
   {
     ksort($this->wordArray);
   }
-
+/**
+ * Returns the array stored in the wordArray Property.
+ * @return array  contains the sequences in the keys, and the orignal words as the values.
+ */
   public function getWordArray()
   {
     return $this->wordArray;
